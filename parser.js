eaa25cf9ffcb4e56a11a6c095d238ea2b110b1cb
liesun.wjb <liesun.wjb@taobao.com>
2015-12-09 17:38:52 +0800

    将expandSSI等函数从util.js文件中移动到parse.js。 2. 优化了锁紧计算逻辑

--- parser.js
+++ parser.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var fs    = require('fs');
+var path  = require('path');
 var util  = require('./util');
-var space = util.space;
 
 /**
  * 将文本解析为由常量、插值、代码块3种token组成的列表.
@@ -11,8 +11,8 @@ var space = util.space;
  * @return  {Array<JSON>}
  */
 function parse(content, conf) {
-    var cont = util.expandSSI(content, conf);
-    var reg  = /(<%=|<%|<!--|%>|-->)/;
+    var cont = expandSSI(content, conf);
+    var reg  = /(<%%|<%=|<%|<!--|-->|%%>|%>)/;
     var start_posi = 0, end_posi = 0, ctx = [], tokens = [];
     var isInCmt = false, cur_depth = 0, cur_type;
 
@@ -71,8 +71,9 @@ function parse(content, conf) {
 
         if(isInCmt) { return; }
 
+        var emap  = {'<%%': '<%', '%%>': '%>'};
         var type  = ctx.length > 0 ? ctx[0].type : 'const';
-        var val   = type == 'insert' ? i.trim() : i;
+        var val   = type == 'insert' ? i.trim() : (emap[i] || i);
         var token = {type: type, val: val, posi: start_posi, depth: cur_depth };
 
         tokens.push(token);
@@ -87,7 +88,6 @@ function parse(content, conf) {
         util.throwParseError('存在未闭合的标签!', cont, ctx[0].posi, conf.file);       
     }
 
-    console.log(tokens);
     return tokens;
 }
 
@@ -98,32 +98,39 @@ function parse(content, conf) {
  * @return  {String}
  */
 function format(tokens, config){
+    var space = util.space;
     var conf = util.merge({ 
         tab_space: 4, 
         func_name: '',
         extra_space: 0, 
         func_arg_name: 'obj',
-        always_wrap_insert: true,
+        output_buff_name : '__bf',
+        always_wrap_insert: false,
         first_line_no_space: false,
     }, config);
 
     var extraSpace = typeof conf.extra_space == 'number' ? space(conf.extra_space) : ''; 
-    var firstLineSpace = conf.first_line_no_space == true ? '' : extra_space;
+    var firstLineSpace = conf.first_line_no_space == true ? '' : space(conf.extra_space);
     var sp8 = extraSpace + space(conf.tab_space * 2);
     var sp4 = extraSpace + space(conf.tab_space);
-    var argName = conf.func_arg_name;
+    var buffName = conf.output_buff_name;
+    var argName  = conf.func_arg_name;
 
-    var cur_depth = 0, buff = [], output = [
+    var preCode = null, cur_depth = 0, buff = [], output = [
         firstLineSpace + 'function ' + conf.func_name + '(' + argName + ') {\n', 
-        sp4 + 'var __buff__ = [];\n\n', 
-        sp4 + 'with(' + argName + '){\n',
-        sp8 + '"use strict";\n'
+        sp4 + 'var '  + buffName + ' = [];\n\n', 
+        sp4 + 'with(' + argName  + '){\n',
+        sp8 + '"use strict";\n\n'
     ];
 
     function clearBuff(){
         if(buff.length == 0) { return; }
 
-        output.push(getConstCode(buff, sp8, conf));
+        if(util.isCtrlStatementEnd(preCode)) { 
+            output.push('\n'); 
+        }
+        
+        output.push(getConstCode(buff, sp8, conf) + '\n');
         buff.length = 0;
     }
 
@@ -133,22 +140,30 @@ function format(tokens, config){
             return;
         }
 
+        var val = translateHereDoc(t.val, conf);
+        var preSpace = sp8 + space(conf.tab_space * (t.depth + t.adjustDetph));
+        var codeCont = util.adjustLeftSpace(val, preSpace, conf);
+        var hasConstBeforeCode = buff.length > 0;
+
         clearBuff();
         
-        var val = t.val;
-        var isMulti  = util.isMultiCont(val);
-        var preSpace = sp8 + space(conf.tab_space * (t.depth + t.adjustDetph));
-        var codeCont = isMulti ? util.translateHereDoc(val, conf) : val.trim();
+        if( hasConstBeforeCode
+            || util.isCtrlStatementStart(t) 
+            || util.isCtrlStatementEnd(preCode)) { 
+    
+            output.push('\n'); 
+        }
         
-        output.push('\n' + util.insertAtLineStart(codeCont, preSpace) + '\n');            
+        preCode = t;
+        output.push(codeCont + '\n');            
     });
 
     clearBuff();
     output.push('\n' + sp4 + '}\n');
-    output.push('\n' + sp4 + 'return __buff__.join("");\n');
+    output.push('\n' + sp4 + 'return ' + buffName + '.join("");\n');
     output.push(extraSpace + "}")
 
-    return output.join('');
+    return util.removeDuplicateBlank(output.join(''));
 }
 
 /**
@@ -160,24 +175,249 @@ function format(tokens, config){
  */
 function getConstCode(tokens, extraSpace, config){
     var conf = util.merge({tab_space: 4}, config);
+    var lineInfo = getConstLines(tokens, conf);
+    var lines = lineInfo.lines.filter(util.skipBlank);
+    var space = util.space;
 
-    var lineInfo = util.parseLines(tokens);
-    var lines = lineInfo.lines;
     if(lines.length == 0) { return ''; }
 
     var depth = tokens[0].depth;
     var minSpace  = lineInfo.minSpace;
+    var buffName  = conf.output_buff_name;
     var preSpace  = extraSpace  + space(conf.tab_space * depth);
     var lineSpace = preSpace    + space(conf.tab_space);
     var fnMap = function(l) { return lineSpace + '+ ' + l.slice(minSpace); };
 
     return (lines.length == 1
-        ? '\n' + preSpace + '__buff__.push(' + lines[0].trim() + ');\n' 
-        : '\n' + preSpace + '__buff__.push(""\n' 
-               + lines.map(fnMap).join('\n') 
-               + '\n' + preSpace + ');\n'
-    );
+        ? (preSpace + buffName + '.push(' + lines[0].trim() + ');')
+        : (preSpace + buffName + '.push(""\n' 
+                + lines.map(fnMap).join('\n') 
+                + '\n' + preSpace + ');'
+          )
+    ) ;
 }
+
+/**
+ * 对将要编译的内容进行预处理，将其中的ssi展开并返回展开后内容.
+ * @param  {String}cont
+ * @param  {Object}ctx
+ * @return {String}
+ */
+function expandSSI(cont, context){
+    var ctx = util.merge({preSpace: '', parents: []}, context);
+    var curFile  = ctx.file;
+    var parents  = ctx.parents;
+    var preSpace = ctx.preSpace;
+    var reg = new RegExp((''
+            + '(?:'                 // 匹配ssi
+            +     '(^\\s+)?'        // ssi前的前导缩进
+            +     '<!--#include\\s+file\\s*=\\s*[\'"](.*?)[\'"]\\s*-->'
+            + ')'
+            + '|(?:(^\\s+)?(.+?)$)' // 匹配不包含ssi的非空单行文本
+        ), 'mgi');
+
+    return cont.replace(reg, function(m, ssiSpace, refFile, lineSpace, lineCont){
+        // 对于不包含ssi的普通行非空行，直接在前面补充空格
+        if(lineCont) { return (lineSpace || '') + preSpace + lineCont; }
+
+        if(!curFile) { util.throwError('<cyan>@expandSSI</cyan>: <red>please add property <cyan>"file"</cyan> for ctx!</red>') }
+        
+        var fileCont, dir = path.dirname(curFile);
+        var myParents = ctx.parents.slice(0);
+        var mySpace  = preSpace + (ssiSpace || '');
+        var filePath = path.resolve(path.join(dir, refFile));
+
+        if(!fs.existsSync(filePath)){ 
+            util.throwError(''
+                + '\n<cyan>@expandSSI</cyan>: '
+                + '<red>the include file <cyan>"' + refFile + '"</cyan> not exists!</red>\n'
+                + '\tcurrent file: <pink>' + filePath + '</pink>\n'
+                + '\treference map:\n' + util.getRefMapStr(refFile, myParents)
+            ); 
+        }
+
+        myParents.unshift(curFile);
+        
+        if(util.hasCircularRef(filePath, myParents)){ 
+            util.reportCircularRefError(filePath, myParents); 
+        }
+
+        fileCont = fs.readFileSync(filePath, 'utf-8');
+        
+        return expandSSI(fileCont, { file: filePath, preSpace: mySpace, parents: myParents });
+    });
+}
+
+/**
+ * 处理代码块中的heredoc.
+ * @param  {String}cont
+ * @return {String}
+ * @example 
+ * // the following code :
+ *      function foo(obj){
+ *          var str = @eof
+ *              <div class="c-item-list-1 c#{obj.prop1} #{obj.prop2}">
+ *                  #{obj.prop1} #{obj.prop2} #{obj.prop3}
+ *                  <div class="c-card">
+ *                      <div class="ctn-wrap chart-wrap j-star-chart-list"></div>
+ *                  </div>
+ *              </div>
+ *          eof, var2 = 123;
+ *
+ *          return str;
+ *      }
+ *  
+ *  // whill be translated to :
+ *      function foo(obj){
+ *          var str = (''
+ *              + '<div class="c-item-list-1 c' + obj.prop1 + ' ' + obj.prop2 + '">'
+ *              +     obj.prop1 + obj.prop2 + obj.prop3
+ *              +     '<div class="c-card">'
+ *              +         '<div class="ctn-wrap chart-wrap j-star-chart-list"></div>'
+ *              +     '</div>'
+ *              + '</div>'
+ *          ), var2 = 123;
+ *          return str;
+ *      }    
+ */
+function translateHereDoc(cont, conf){
+    var findReg = new RegExp((''
+            + '(/\\*[\\S\\s]*?\\*/)'  // multiline comment
+            + '|(//.*?$)'             // single line comment
+            + '|(/.*?/\\w+)'          // regexp 
+            + '|([\'"].*?[\'"])'      // string
+            + '|(?:@(\\w+)([\\S\\s]*?)(^\\s*)?\\5(?=\\b))' // heredoc conent
+        ), 'mg');
+
+    return cont.replace(findReg, function(match, mc, sc, reg, str, docStart, docCont, endSpace){
+        if(mc || sc || reg || str) { return match; }
+
+        var insertReg = /(#\{.*?\})/;
+        var tokens = docCont.split(insertReg).map(function(i){
+            return (insertReg.test(i)
+                ? { type: 'insert', val: i.slice(2, -1) }
+                : { type: 'const' , val: i }
+            );
+        });
+
+        var lineInfo = getConstLines(tokens, conf);
+        var lines = lineInfo.lines;
+        if(lines.length == 0) { return ''; }
+
+        var minSpace = lineInfo.minSpace;
+        var preSpace = util.space(minSpace);
+        var fnMap = function(l) { return preSpace + '+ ' + l.slice(minSpace); };     
+        
+        return '(""\n' 
+            + lines.map(fnMap).join('\n') 
+            + '\n' + (endSpace || '') + ')';
+    });
+}
+
+/**
+ * 将tokens序列解析为行数据并计算出多行的最小缩进.
+ * @param  {Array<JSON>}tokens
+ * @parma  {JSON}conf
+ * @return Array<JSON>
+ */
+function getConstLines(tokens, conf){
+    var config = util.merge({ tab_space : 4}, conf);
+    var tabSpace = util.space(config.tab_space), lines = [];
+    var alwaysWrapInsert = config.always_wrap_insert == true;
+    var minSpace, spaceCounts = [], estr = util.estr;
+    
+    var lines = tokens.map(function(t){ 
+            return t.type == 'insert' ? '<%=' + t.val + '%>' : t.val; 
+        }).join('').split(/\n/).filter(util.skipBlank);
+
+    lines = lines.map(function(l){
+        // 保留行首空格，替换tab, 去掉行末空格
+        l = l.replace(/\t/g, tabSpace).replace(/\s+$/, '');
+
+        var insertReg = /(<%=.*?%>)/;
+        var propReg = new RegExp(''
+                + '^[$\\w]+'  // 第一个属性
+                + '(?:'       // 其后若干通过点或中括号引用的其他属性
+                +     '\\s*\\.\\s*[$\\w]+\\s*'
+                +     '|\\s*\\[\\s*[\'"$\\w]+\\s*\\]\\s*'
+                + ')*$'
+            );
+
+        var match = l.match(/^\s+/);
+        var preSpace = match ? match[0] : '';
+        var lineCont = l.slice(preSpace.length);
+        var parts = lineCont.split(insertReg).filter(util.skipEmpty);
+
+        var result = parts.map(function(i, idx){
+            var cont, lastChar, isTag, isProp, isUTF8Char;
+            var isLast   = idx == parts.length - 1;
+            var isInsert = insertReg.test(i);
+           
+            // 对于插值，如果"<%=" 与 "%>" 之间的内容为简单的属性调用
+            // 直接返回，否则在内容外面添加括号，以防止出现下面的情况:
+            // <%= prop1 + 1%><%= prop2 %>被翻译为
+            // prop1 + 1 + prop2, 这与作者的原意不相符。
+            // 检测是否为属性读取语句是通过正则判断的，有一定的可能误判，
+            // 如果读者想要精确的结果，可以通过conf.always_wrap_insert = true
+            // 来强制总是在插值外层添加括号
+            if(isInsert) { 
+                cont = i.slice(3, -2).trim();
+                if(alwaysWrapInsert) { return ' ( ' + cont + ' ) '; }
+                
+                isProp = propReg.test(cont);
+                return isProp ? cont : ' ( ' + cont + ' ) '; 
+            }
+            
+            // 对于多行html来说有时候需要在两行内种直接添加必要的空格
+            // 比如像下面这种情况：
+            //    <div id="div1" name="div1"
+            //        class="foo"/>
+            // 如果两行之间的空格都背去掉了，就会变成这样：
+            //    <div id="div1" name="div1"class="foo"/>
+            // 很明显这是错误的。但有时候行直接的空格是累赘或多于的
+            // 比如像下面这样:
+            //    <div id="div1" name="div1">
+            //        <span>fff</span>
+            //    </div>
+            // 或者是大段的文本内容。多于的空格会造成文本直接多出一个
+            // 一个空格，影响整体的显示效果。因此是否需要在行末添加空格
+            // 需要区别对待。在本程序中为了方便处理，我们按照如下逻辑来判断
+            // 是否需要添加空格：如果最后一个非插值元素的最后一个字符是">"
+            // 或非ASC字符(比如中文内容特殊符号等)不在最后加空格，
+            // 否则在最后添加空格。
+            // 
+            // 注意这个条件语句顺序需要在判读是否为插值的语句之后，否则会出现将
+            // 最后一个插值元素后面添加空格的误判。
+            if(isLast) {
+                lastChar = i.charAt(i.length -1);
+                isUTF8Char = /[\u0100-\uffff]/.test(lastChar);
+                isTag = lastChar == '>';
+
+                return (isTag || isUTF8Char) ? estr(i) : estr(i + ' ');
+            }
+
+            return estr(i);
+        });
+
+        
+        if(l.trim().length > 0){ 
+            spaceCounts.push(preSpace.length); 
+        }
+        
+        return preSpace + result.join(' + ');
+    });
+
+
+    minSpace = (spaceCounts.length > 0 
+        ? Math.min.apply(Math, spaceCounts) 
+        : 0
+    );
+    
+    return { lines: lines, minSpace : minSpace };     
+}
+
+
+// export ----------------------------------------------------
 
 exports.parse = function(cont, conf){
     return format(parse(cont, conf), conf);
--- test/test-parser.js
+++ test/test-parser.js
@@ -1,12 +1,13 @@
 var fs = require('fs');
 var parser = require('../parser');
-var file = __dirname + "/tmpl/proj-overview.html";
+//var file = __dirname + "/tmpl/proj-overview.html";
+var file = __dirname + "/tmpl-2.html";
 var cont = fs.readFileSync(file, 'utf-8');
 var code = parser.parse(cont, {
     file: file,
     tab_space: 4, 
-    extra_space: 4, 
-    first_line_no_space: true
+    extra_space: 0, 
+    first_line_no_space: false
 });
 
-console.log('    tmpl: ' + code);
+console.log(code);
