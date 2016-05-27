46bd070218413a1d082d6f2ee8850fd0eef29a76
liesun.wjb <liesun.wjb@taobao.com>
2016-05-27 14:49:43 +0800

    添加了sjtc命令支持

--- bin/sjtc.js
+++ bin/sjtc.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 
-var cmd = require('commander');
+var fs   = require('fs');
+var cmd  = require('commander');
+var sjtc = require('../src/parser');
 
-cmd .version('0.0.1')
-    .option('-f, --format', 'kissy or require')
-    .option('-it, --input-tab-size', 'input tab size default 4')
-    .option('-ot, --output-tab-size', 'output-tab-size default 4')
+cmd .version('0.0.6')
+    .allowUnknownOption()
+    .usage('[options] <source> <target>')
+    .option('-fns, --first_line_no_space <true|false>')
+    .option('-its, --input_tab_space  <n>', 'input tab space default 4', parseInt)
+    .option('-ots, --output_tab_space <n>', 'output-tab-space default 4', parseInt)
     .parse(process.argv);
+
+
+if(cmd.args.length < 2){
+    cmd.help();
+}else{
+    var source = cmd.args[0];
+    var target = cmd.args[1];
+    var conf = {
+        input_tab_space     : cmd.input_tab_space  || 4,
+        output_tab_space    : cmd.output_tab_space || 4,
+        first_line_no_space : cmd.first_line_no_space == true
+    };
+
+    fs.writeFileSync(target, sjtc.parse(source, conf));
+}
